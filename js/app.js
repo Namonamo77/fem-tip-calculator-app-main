3098ba01a7a92074a82ed7346bbcdfb73d52c3a7
Namonamo <kactusworld.design@gmail.com>
2023-06-27 08:43:03 +0200

    Added error states in js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let amountPerPerson = document.querySelector(".amount-value")
 let totalPerPerson = document.querySelector(".total-value")
 let resetBtn = document.querySelector(".reset-cta")
 let customInput = document.querySelector(".tip-input")
+let errMessage = document.querySelector(".err-people")
 
 let amountValue = 0
 let calculateTip = 0
@@ -26,21 +27,43 @@ function calculateTotalPerClient(){
 
 const tipValue = (e) =>{
     amountValue = e.target.value
+
+    tipAmount.forEach(tipBtn => {
+        tipBtn.classList.remove("active")
+        if (tipBtn.value === amountValue){
+            tipBtn.classList.add("active")
+        }
+    })
     console.log(amountValue)
 
     calculateTipPerClient()
 
     calculateTip = customerShareAmount * amountValue / 100
-    amountPerPerson.textContent = `$${calculateTip}`
-
     calculateTotalPerClient()
-    totalPerPerson.textContent = `$${totalAmount}`
+
+    if (customerNumber.value <= 0) {
+        customerNumber.classList.add("error")
+        errMessage.style.display = "block"
+         return (
+            (amountPerPerson.textContent = "$0.00") 
+            (totalPerPerson.textContent = "$0.00")
+            )
+    }else {
+        customerNumber.classList.remove("error")
+        errMessage.style.display = "none"
+    }
     
+    if (calculateTip > 0 && totalAmount > 0){
+        amountPerPerson.textContent = `$${calculateTip.toFixed(2)}`
+        totalPerPerson.textContent = `$${totalAmount.toFixed(2)}`
+    }
         
 }
 
+
 for (let i = 0; i < tipAmount.length; i++){
     tipAmount[i].addEventListener("click", tipValue)
+   
 }
 
 
@@ -50,11 +73,15 @@ function reset(){
     totalAmount = ""
     customerShareAmount = ""
 
-    amountPerPerson.textContent = "$0"
-    totalPerPerson.textContent = "$0"
+    amountPerPerson.textContent = "$0.00"
+    totalPerPerson.textContent = "$0.00"
     customerNumber.value = "0"
     billInput.value = 0
     customInput.value = "Custom"
+    tipAmount.forEach(tipBtn => tipBtn.classList.remove("active"))
+
+    customerNumber.classList.remove("error")
+    errMessage.style.display = "none"
 }
 
 resetBtn.addEventListener("click", reset)
